c42375f9245bffcef375a342d7f3ee3c9c2338e8
JulianoZ <juliano_z@yahoo.com.br>
2026-09-09 13:02:51 -0300

    feat: adiciona pagina de politica da empresa (#2)

--- src/components/SistemaRotas.jsx
+++ src/components/SistemaRotas.jsx
@@ -5,6 +5,7 @@ import Sobre from "../pages/Sobre"
 import Contato from "../pages/Contato"
 import  Produto from "../pages/Produto"
 import Servico from "../pages/Servico"
+import PoliticaEmpresa from "../pages/PoliticaEmpresa"
 
 function SistemaRotas() {
   return (
@@ -14,6 +15,7 @@ function SistemaRotas() {
       <Route path="/contato" element={<Contato />} />
       <Route path="/produto" element={<Produto />} />
       <Route path="/servico" element={<Servico />} />
+      <Route path="/politica-empresa" element={<PoliticaEmpresa />} />
     </Routes>
     
   )
