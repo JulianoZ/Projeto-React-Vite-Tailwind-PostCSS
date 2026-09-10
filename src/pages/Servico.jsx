15a5cc2939b54c46d8023eb29d2b44b45009e8af
JulianoZ <juliano_z@yahoo.com.br>
2026-09-10 09:55:01 -0300

    feat: atualizar-pagina-servico (#3)

--- src/pages/Servico.jsx
+++ src/pages/Servico.jsx
@@ -2,7 +2,22 @@ function Servico(){
 
 return(
 
-<div> Nova página de Serviço </div>
+<div> Nova página de Serviço <br />
+
+Listagens de serviços <br />
+<ul>
+ <li> Serviço 1</li>
+ <li> Serviço 2</li>
+ <li> Serviço 3</li>
+ <li> Serviço 4</li>
+ <li> Serviço 5</li>
+</ul>
+
+
+    
+    
+    
+     </div>
 
 )
 }export default Servico
